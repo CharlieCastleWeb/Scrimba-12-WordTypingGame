f4d156b1e5abab1b7c506df63d5004cae65083f6
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2022-04-22 11:17:07 +0200

    Project Completed

---
+++ src/useTyping.js
@@ -0,0 +1,61 @@
+import {useState, useEffect, useRef} from "react"
+
+function useTyping(defaultTime = 5) {
+
+    const STARTING_TIME = defaultTime
+  
+    const [text,setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
+    const [wordCount, setWordCount] = useState(0)
+
+    const textBoxRef = useRef(null)
+
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+    }
+
+    function calculateWordCount(text) {
+        const wordsArr = text.trim().split(" ")
+        const filteredWords = wordsArr.filter(word => word !== "")
+        console.log(filteredWords.length)
+        return filteredWords.length
+    }
+
+    function startGame() {
+        setIsTimeRunning(true)
+        setTimeRemaining(STARTING_TIME)
+        setWordCount(0)
+        setText("")
+        textBoxRef.current.disabled = false
+        textBoxRef.current.focus()
+    }
+
+    function endGame() {
+        setIsTimeRunning(false)
+        setWordCount(calculateWordCount(text))
+    }
+
+    useEffect(() => {
+        if (timeRemaining > 0 && isTimeRunning) {
+        setTimeout(() => {
+            setTimeRemaining(time => time -1)
+        }, 1000)
+        } else if(timeRemaining === 0) {
+        endGame()
+        }
+    }, [timeRemaining, isTimeRunning])
+
+    return {
+        textBoxRef, 
+        handleChange, 
+        text, 
+        isTimeRunning, 
+        timeRemaining, 
+        startGame, 
+        wordCount
+        }
+}
+
+export default useTyping
